6d5bdab66b3f3e4801f91eb58bebe308e5d79a9e
NBalboa <nickojek2x@gmail.com>
2025-09-05 04:30:08 +0800

    please work

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! this is the result';
+    return 'Hello World! this is the result please work';
   }
 }

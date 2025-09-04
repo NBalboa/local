1f188679290270602616b49b68a8dac7a2887afd
Nicko Balboa <76141006+NBalboa@users.noreply.github.com>
2025-09-05 02:00:17 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log('test hello world');
+  console.log('test hello world test');
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();

1c6780dfd4bc59b6363ca8e01bf74ed3a4eff4ee
NBalboa <nickojek2x@gmail.com>
2025-09-05 08:20:36 +0800

    hello world

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log(
-    'please work buddy here',
-  );
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();

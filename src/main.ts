6febcf7d3d9cd76c7ddc11faea81c326969d76a8
NBalboa <nickojek2x@gmail.com>
2025-09-05 08:12:37 +0800

    please work bai

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   console.log(
-    'please work buddy',
+    'please work buddy here',
   );
   await app.listen(process.env.PORT ?? 3001);
 }

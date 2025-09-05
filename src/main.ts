2a024d5ca1586f41744c59bdcf0e71a1e7845099
NBalboa <nickojek2x@gmail.com>
2025-09-05 08:02:59 +0800

    here

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   console.log(
-    'test',
+    'please work buddy',
   );
   await app.listen(process.env.PORT ?? 3001);
 }
